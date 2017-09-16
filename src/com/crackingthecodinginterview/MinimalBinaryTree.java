9349eb7896314af1c9746099d49b579a83583012
rakshitsareen <rakshitsareen@172-16-26-39.dynapool.nyu.edu>
2017-09-16 16:29:07 -0400

    creation of minimal tree from sorted array

---
+++ src/com/crackingthecodinginterview/MinimalBinaryTree.java
@@ -0,0 +1,58 @@
+package com.crackingthecodinginterview;
+
+class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int val) {
+		value = val;
+		left = null;
+		right = null;
+	}
+}
+
+class Tree {
+	TreeNode root;
+}
+
+public class MinimalBinaryTree {
+
+	// node fun(array,m,n)
+	// if array.size == 1;
+	// return new Node(array[0]);
+	// mid = m + (n-m+1)/2;
+	// root.value = array[mid];
+	// root->left = fun(0,mid-1);
+	// root->right = fun(mid+1,n);
+
+	public TreeNode createMinimal(int[] array, int m, int n) {
+		if (m == n)
+			return new TreeNode(array[m]);
+		if (n < m)
+			return null;
+
+		int mid = (m + n) / 2;
+
+		TreeNode root = new TreeNode(array[mid]);
+		root.left = createMinimal(array, m, mid - 1);
+		root.right = createMinimal(array, mid + 1, n);
+		return root;
+	}
+
+	public void inorder(TreeNode root) {
+		if (root == null)
+			return;
+		inorder(root.left);
+		System.out.println(root.value);
+		inorder(root.right);
+	}
+
+	public static void main(String[] args) {
+		int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		MinimalBinaryTree sol = new MinimalBinaryTree();
+		TreeNode root = sol.createMinimal(sortedArray, 0, sortedArray.length - 1);
+		sol.inorder(root);
+		return;
+	}
+}
