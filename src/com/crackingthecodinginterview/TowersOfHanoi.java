b6514a3c021b120ea62a99df0c530b2cdb651e00
rakshitsareen <rakshitsareen@172-16-30-204.dynapool.nyu.edu>
2017-10-04 18:11:35 -0400

    Recursive Towers of Hanoi

---
+++ src/com/crackingthecodinginterview/TowersOfHanoi.java
@@ -0,0 +1,24 @@
+package com.crackingthecodinginterview;
+
+public class TowersOfHanoi {
+
+	public static void towersOfHanoi(int nDisks, char from, char to, char aux) {
+		if (nDisks == 1) {
+			System.out.println("Move disk 1 from rod " + from + " to rod " + to);
+			return;
+		}
+		
+		else {
+			towersOfHanoi(nDisks-1, from, aux, to);
+			System.out.println("Move disk "+nDisks+" from rod " + from + " to rod " + to);
+			towersOfHanoi(nDisks-1, aux, to, from);
+			return;
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 4; // Number of disks
+		towersOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
+	}
+
+}
