e4d097bc05d5a3a2d5614c51abd634c4d3056404
rakshitsareen <rs5606@nyu.edu>
2018-06-06 18:42:22 -0400

    valid number - todo : complete.

---
+++ src/com/leetcode/ValidNumber.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+public class ValidNumber {
+
+	public boolean isNumber(String s) {
+		if (s != null) {
+			s = s.trim();
+			if (!s.isEmpty()) {
+				for(int i = 0; i <s.length(); i++) { // check every position for consistency
+					
+				}
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
