7a68658e4f5a9a187dcfac9d503b79722072d929
Rakshit Sareen <rs5606@nyu.edu>
2018-08-25 09:14:24 -0400

    Jump Game with TLE

---
+++ src/com/leetcode/JumpGame.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+public class JumpGame {
+
+	// TLE problem
+	public boolean canJumpUtil(int[] nums, int start, int end) {
+		if (start == end)
+			return true;
+		if (start > end)
+			return false;
+		for (int jump = nums[start]; jump > 0; jump--) {
+			// check if this jump can do it.
+			if (canJumpUtil(nums, start + jump, end))
+				return true;
+		}
+		return false;
+	}
+
+	// TLE problem
+	public boolean canJump(int[] nums) {
+		int len = nums.length;
+		if (len == 1)
+			return true;
+		boolean allzero = true;
+		boolean no_zero_at_all = true;
+		for (int i = 0; i < len; i++) {
+			if (nums[i] != 0)
+				allzero = false;
+		}
+		if (allzero)
+			return false;
+		for (int i = 0; i < len; i++) {
+			if (nums[i] == 0)
+				no_zero_at_all = false;
+		}
+		if (no_zero_at_all)
+			return true;
+		// there is at least one zero in the array.
+		return canJumpUtil(nums, 0, len - 1);
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 3, 1, 1, 4 };
+		JumpGame sol = new JumpGame();
+		System.out.println(sol.canJump(nums));
+	}
+
+}
