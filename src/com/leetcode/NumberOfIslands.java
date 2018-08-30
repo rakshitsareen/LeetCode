d9a7a5ac401157c373bf711945cdfcc61519cc80
Rakshit Sareen <rs5606@nyu.edu>
2018-08-30 16:56:10 -0400

    TOOD: number of islands

--- src/com/leetcode/NumberOfIslands.java
+++ src/com/leetcode/NumberOfIslands.java
@@ -1,13 +1,16 @@
 package com.leetcode;
 
 public class NumberOfIslands {
-	
+
+	public void sink(int i, int j, int[][] grid) {
+	}
+
 	public int numIslands(char[][] grid) {
-		
+
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 	}
 }
