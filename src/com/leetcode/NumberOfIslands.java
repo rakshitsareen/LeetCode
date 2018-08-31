d15dfec38bbe2a190ef8fa84eeb2888cd23e7a02
Rakshit Sareen <rs5606@nyu.edu>
2018-08-31 02:41:55 -0400

    number of islands

--- src/com/leetcode/NumberOfIslands.java
+++ src/com/leetcode/NumberOfIslands.java
@@ -2,15 +2,28 @@ package com.leetcode;
 
 public class NumberOfIslands {
 
-	public void sink(int i, int j, int[][] grid) {
+	public int sink(int i, int j, char[][] grid) {
+		if (i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == '0')
+			return 0;
+		grid[i][j] = '0'; // sink this island
+		sink(i + 1, j, grid);
+		sink(i, j + 1, grid);
+		sink(i - 1, j, grid);
+		sink(i, j - 1, grid);
+		return 1;
 	}
 
 	public int numIslands(char[][] grid) {
-
-		return 0;
+		int islands = 0;
+		for (int i = 0; i < grid.length; i++)
+			for (int j = 0; j < grid[i].length; j++)
+				islands += sink(i, j, grid);
+		return islands;
 	}
 
 	public static void main(String[] args) {
-
+		char[][] grid = { { '1', '1', '0', '0', '0' }, { '1', '1', '0', '0', '0' }, { '0', '0', '1', '0', '0' },
+				{ '0', '0', '0', '1', '1' } };
+		System.out.println(new NumberOfIslands().numIslands(grid));
 	}
 }
