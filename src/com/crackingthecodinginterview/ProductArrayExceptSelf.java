5e9ba291d47a3e6180b71a633e7dfefa8c600c6b
Rakshit Sareen <rs5606@nyu.edu>
2018-09-10 20:00:23 -0400

    ProductArrayExceptSelf.java

---
+++ src/com/crackingthecodinginterview/ProductArrayExceptSelf.java
@@ -0,0 +1,44 @@
+package com.crackingthecodinginterview;
+
+public class ProductArrayExceptSelf {
+
+	public static int[] productExceptSelf(int[] nums) {
+		if (nums == null)
+			return null;
+		if (nums.length == 0)
+			return nums;
+		if (nums.length == 1) {
+			if (nums[0] == 1)
+				return nums;
+			if (nums[0] != 1) {
+				int[] ans = { 1 };
+				return ans;
+			}
+		}
+		int[] ltor = new int[nums.length];
+		int[] rtol = new int[nums.length];
+		ltor[0] = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			ltor[i] = ltor[i - 1] * nums[i];
+		}
+		rtol[nums.length - 1] = nums[nums.length - 1];
+		for (int i = nums.length - 2; i >= 0; i--) {
+			rtol[i] = rtol[i + 1] * nums[i];
+		}
+		int[] ans = new int[nums.length];
+		ans[0] = rtol[1];
+		for (int i = 1; i < nums.length - 1; i++) {
+			ans[i] = ltor[i - 1] * rtol[i + 1];
+		}
+		ans[nums.length - 1] = ltor[nums.length - 2];
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4 };
+		int[] ans = productExceptSelf(nums);
+		for (int i : ans)
+			System.out.println(i);
+	}
+
+}
