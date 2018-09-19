092ac523bfb44eabe6810d68af8d3aa7fced4009
Rakshit Sareen <rs5606@nyu.edu>
2018-09-19 01:08:51 -0400

    finally corrected it

--- src/com/leetcode/Twitter.java
+++ src/com/leetcode/Twitter.java
@@ -1,50 +1,35 @@
 package com.leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Twitter {
 
-	public static List<String> missingWords(String s, String t) {
+
+	public static List<String> _missingWords(String s, String t) {
 		List<String> output = new ArrayList<>();
-		if (s == null)
-			return null;
 		String[] _s = s.split(" ");
-		if (t == null || t.length() == 0) {
-			for (String g : _s) {
-				if (g.trim().length() == 0)
-					continue;
-				output.add(g);
-			}
-			return output;
-		}
-		// t is subsequence of s .
-		if (_s.length == 0)
-			return new ArrayList<>();
 		String[] _t = t.split(" ");
-		Set<String> superset = new LinkedHashSet<>();
-		for (String res : _s) {
-			if (res.trim().length() == 0)
-				continue;
-			superset.add(res);
+		int k = 0;
+		for (int i = 0; i < _t.length; ++i) {
+			String in = _t[i];
+			while (!_s[k].equals(in) && k < _s.length) {
+				k++;
+			}
+			if (k < _s.length) {
+				_s[k] = null;
+			}
+			k++;
 		}
-		// superset.addAll(Arrays.asList(_s));
-		for (String subsequence : _t) {
-			if (subsequence.trim().length() == 0)
-				continue;
-			superset.remove(subsequence);
-		}
-		for (String result : superset) {
-			output.add(result.trim());
+		for (String out : _s) {
+			if (out != null)
+				output.add(out);
 		}
 		return output;
 	}
 
 	public static void main(String[] args) {
-		List<String> ans = missingWords("I love programming also", "love Also");
+		List<String> ans = _missingWords("I am using HackerRank to improve programming", "am HackerRank to improve");
 		for (String a : ans)
 			System.out.println(a);
 	}
