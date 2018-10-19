e3c1a5f3438ae87e8ec3ce4af50e02384228df0e
Rakshit Sareen <rs5606@nyu.edu>
2018-10-19 00:41:09 -0400

    FruitIntoBaskets.java

---
+++ src/com/leetcode/FruitIntoBaskets.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitIntoBaskets {
+
+	public static int totalFruit(int[] tree) {
+		if (tree == null)
+			return 0;
+		if (tree.length == 0)
+			return 0;
+		Map<Integer, Integer> map = new HashMap<>(); // map to save indices of types encountered. will save latest
+														// indices of types encountered.
+		int leftmost = 0, rightmost = 0, maxlen = 0, start = 0;
+		while (rightmost < tree.length) {
+			if (map.size() <= 2) {
+				map.put(tree[rightmost], rightmost);
+				rightmost++;
+			}
+			if (map.size() > 2) {
+				leftmost = tree.length;
+				for (int i : map.values())
+					leftmost = Math.min(leftmost, i);
+				map.remove(tree[leftmost]);
+				start = leftmost + 1;
+			}
+			maxlen = Math.max(maxlen, rightmost - start);
+		}
+		return maxlen;
+	}
+
+	public static void main(String[] args) {
+		int[] tree = { 3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4 };
+		System.out.println(totalFruit(tree));
+	}
+
+}
--- src/com/leetcode/ItrUseCase.java
+++ src/com/leetcode/ItrUseCase.java
@@ -1,9 +1,9 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
 
 // how to use an Iterator, student asked this one
 public class ItrUseCase {
@@ -16,16 +16,37 @@ public class ItrUseCase {
 		mlist.add(1);
 		mlist.add(2);
 		mlist.add(3);
+		mlist.add(4);
 	}
 
+//	public MovieList getMatchingTitles(String keyword) {
+//		if (keyword == null || keyword.length() == 0)
+//			return null;
+//		
+//		MovieList m = new MovieList();
+//		
+//		for(Object s : this.elementData) {
+//			String stemp = (String)s;
+//			if(stemp.contentEquals(keyword)) {
+//				m.add(stemp);
+//			}
+//		}
+//		if(m.size() > 0) {
+//			return Collections.sort(m);
+//		}
+//		
+//		return null;
+//	}
+
 	public void iterate() {
+
+		Integer.parseInt("five");
+		Iterator<Integer> xitr = mlist.iterator();
+		if (xitr.hasNext())
+			System.out.println(xitr.next());
 		Iterator<Integer> mitr = mlist.iterator();
 		while (mitr.hasNext()) {
 			System.out.println(mitr.next());
-		}
-
-		for (Integer e : mlist) {
-			System.out.println(e);
 		}
 	}
 
