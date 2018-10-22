cfd45db6890692a67f224ef40680d89f1c76b893
Rakshit Sareen <rs5606@nyu.edu>
2018-10-22 16:16:35 -0400

    Candy.java

---
+++ src/com/leetcode/Candy.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class Candy {
+
+	public static int candy(int[] ratings) {
+		int[] candies = new int[ratings.length];
+		candies[0] = 1;
+		for (int i = 1; i < ratings.length; i++) {
+			candies[i] = 1;
+			if (ratings[i] > ratings[i - 1])
+				candies[i] = candies[i - 1] + 1;
+		}
+		int sum = candies[candies.length - 1];
+		for (int i = ratings.length - 2; i >= 0; i--) {
+			if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
+				candies[i] = candies[i + 1] + 1;
+			sum += candies[i];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] ratings = { 1, 2, 2 };
+		System.out.println(candy(ratings));
+	}
+
+}
