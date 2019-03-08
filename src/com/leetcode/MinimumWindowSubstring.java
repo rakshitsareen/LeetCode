2632e253df500a301e8eac1fb8f08367e18c7932
Rakshit Sareen <rs5606@nyu.edu>
2019-03-08 14:56:54 -0500

    src/com/leetcode/MinimumWindowSubstring.java

--- src/com/leetcode/MinimumWindowSubstring.java
+++ src/com/leetcode/MinimumWindowSubstring.java
@@ -1,50 +1,41 @@
 package com.leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MinimumWindowSubstring {
-
-	public String minWindow(String s, String t) {
+	
+	public String minWindowCorrect(String s, String t) {
 		if (s == null || s.length() == 0 || t == null || t.length() == 0)
 			return "";
-
-		HashMap<Character, Integer> map = new HashMap<>();
-		HashMap<Character, Integer> dict = new HashMap<>();
-
-		// populate the dict with t's character , frequency
-		for (int i = 0; i < t.length(); i++) {
-			map.put(t.charAt(i), 0);
-			if (dict.containsKey(t.charAt(i)))
-				dict.put(t.charAt(i), dict.get(t.charAt(i)) + 1);
-			else
-				dict.put(t.charAt(i), 1);
-		}
-
-		int start = 0;
-		int minlength = s.length() + 1;
-		String result = "";
-		int count = 0;
-
-		for (int end = 0; end < s.length(); end++) {
-			if (map.containsKey(s.charAt(end))) {
-				map.put(s.charAt(end), map.get(s.charAt(end)) + 1);
-				if (map.get(s.charAt(end)) <= dict.get(s.charAt(end)))
-					count++;
+		int begin = 0, end = 0;
+		int finalbegin = begin, finalend = end;
+		int len = Integer.MAX_VALUE;
+		Map<Character, Integer> map = new HashMap<>();
+		for(Character c : t.toCharArray())
+        	map.put(c, map.getOrDefault(c, 0) + 1);
+		int counter = map.size();
+		while(end < s.length()) {
+			if(map.containsKey(s.charAt(end))) {
+				map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
+				if(map.get(s.charAt(end)) == 0) counter--;
 			}
-
-			if (count == t.length()) {
-				while (!dict.containsKey(s.charAt(start)) || map.get(s.charAt(start)) > dict.get(s.charAt(start))) {
-					if (map.containsKey(s.charAt(start)))
-						map.put(s.charAt(start), map.get(s.charAt(start) + 1));
-					start++;
+			end++;
+			while(counter == 0) {
+				if(map.containsKey(s.charAt(begin))) {
+					map.put(s.charAt(begin), map.get(s.charAt(begin)) + 1);
+					if(map.get(s.charAt(begin)) > 0) counter++;
 				}
-				if (end - start + 1 < minlength) {
-					minlength = end - start + 1;
-					result = s.substring(start, end - start + 1);
+				if(end - begin + 1 < len) {
+					len = end - begin + 1;
+					finalbegin = begin;
+					finalend = end;
 				}
+				begin++;
 			}
 		}
-		return result;
+		if(len == Integer.MAX_VALUE) return "";
+		return s.substring(finalbegin, finalend);
 	}
 
 	public static void main(String[] args) {
