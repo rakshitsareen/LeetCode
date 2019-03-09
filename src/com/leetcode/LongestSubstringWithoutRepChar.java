3f3ecc9ad5515566becb385ec87a17a335be2e5a
Rakshit Sareen <rs5606@nyu.edu>
2019-03-09 01:12:03 -0500

    src/com/leetcode/LongestSubstringWithoutRepChar.java

--- src/com/leetcode/LongestSubstringWithoutRepChar.java
+++ src/com/leetcode/LongestSubstringWithoutRepChar.java
@@ -1,9 +1,33 @@
 package com.leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepChar {
+	
+	public int lengthOfLongestSubstringWithTemplate(String s) {
+		if (s == null || s.length() == 0)
+			return 0;
+		int maxLen = Integer.MIN_VALUE;
+		int begin = 0, end = 0;
+		int counter = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		while(end < s.length()) {
+			map.put(s.charAt(end), map.getOrDefault(s.charAt(end), 0) + 1);
+			if(map.get(s.charAt(end)) > 1) counter++;
+			end++;
+			while(counter > 0) {
+				char c = s.charAt(begin);
+				if(map.get(c) > 1) counter--;
+				map.put(c, map.get(c) - 1);
+				begin++;
+			}
+			maxLen = Math.max(maxLen, end - begin);
+		}
+		return maxLen;
+	}
 
 	public int lengthOfLongestSubstring(String s) {
 		if (s == null || s.length() == 0)
@@ -36,5 +60,6 @@ public class LongestSubstringWithoutRepChar {
 		
 		LongestSubstringWithoutRepChar sol = new LongestSubstringWithoutRepChar();
 		System.out.println(sol.lengthOfLongestSubstring("abcabcbb"));
+		System.out.println(sol.lengthOfLongestSubstringWithTemplate("abcabcbb"));
 	}
 }
