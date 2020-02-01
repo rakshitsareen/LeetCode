83e2ffa4dae9ccf674377fa16d6c39b7bd189214
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-01 00:27:54 -0500

    rotting oranges - bfs

--- src/com/leetcode/RottingOranges.java
+++ src/com/leetcode/RottingOranges.java
@@ -1,51 +1,43 @@
 package com.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RottingOranges {
 
-	static class Pair {
-		Integer x;
-		Integer y;
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((x == null) ? 0 : x.hashCode());
-			result = prime * result + ((y == null) ? 0 : y.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Pair other = (Pair) obj;
-			if (x == null) {
-				if (other.x != null)
-					return false;
-			} else if (!x.equals(other.x))
-				return false;
-			if (y == null) {
-				if (other.y != null)
-					return false;
-			} else if (!y.equals(other.y))
-				return false;
-			return true;
-		}
-
-		public Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
 	public int orangesRotting(int[][] grid) {
-		return -1;
+		if (grid == null || grid.length == 0)
+			return 0;
+		Queue<int[]> q = new LinkedList<>();
+		int fresh = 0;
+		for (int i = 0; i < grid.length; ++i)
+			for (int j = 0; j < grid[0].length; ++j)
+				if (grid[i][j] == 2) {
+					q.offer(new int[] { i, j });
+				} else if (grid[i][j] == 1) {
+					fresh++;
+				}
+		if (fresh == 0)
+			return 0;
+		int days = 0;
+		int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+		while (!q.isEmpty()) {
+			days++;
+			int size = q.size();
+			for (int i = 0; i < size; ++i) {
+				int[] point = q.poll();
+				for (int[] dir : dirs) {
+					int x = point[0] + dir[0];
+					int y = point[1] + dir[1];
+					if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 2 || grid[x][y] == 0)
+						continue;
+					grid[x][y] = 2;
+					q.offer(new int[] { x, y });
+					fresh--;
+				}
+			}
+		}
+		return fresh == 0 ? days - 1 : -1;
 	}
 
 	public static void main(String[] args) {
