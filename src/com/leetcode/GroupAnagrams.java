d81445178caf074f37a7cc400394250561bd49d8
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-16 16:26:53 -0500

    src/com/leetcode/GroupAnagrams.java

---
+++ src/com/leetcode/GroupAnagrams.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+
+	public List<List<String>> groupAnagrams(String[] strs) {
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		for (String s : strs) {
+			char[] ch = s.toCharArray();
+			Arrays.sort(ch);
+			String key = String.valueOf(ch);
+			if (!map.containsKey(key))
+				map.put(key, new ArrayList<String>());
+			map.get(key).add(s);
+		}
+		return new ArrayList<List<String>>(map.values());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
