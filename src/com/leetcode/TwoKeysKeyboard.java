95c4c6985c83df9e1a7d2ff0f9451c2998e84963
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-16 17:44:16 -0500

    src/com/leetcode/TwoKeysKeyboard.java

---
+++ src/com/leetcode/TwoKeysKeyboard.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class TwoKeysKeyboard {
+
+	public int minSteps(int n) {
+		int[] dp = new int[n + 1];
+		for (int i = 2; i <= n; ++i) {
+			dp[i] = i;
+			for (int j = i - 1; j > 1; --j) {
+				if (i % j == 0) {
+					dp[i] = dp[j] + (i / j);
+					break;
+				}
+			}
+		}
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
