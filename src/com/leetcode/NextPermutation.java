d16bd96a308784dbccc832fe972fe0a7ff42de63
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-17 18:28:13 -0500

    src/com/leetcode/NextPermutation.java

--- src/com/leetcode/NextPermutation.java
+++ src/com/leetcode/NextPermutation.java
@@ -1,10 +1,8 @@
 package com.leetcode;
 
-import java.util.Arrays;
-
 public class NextPermutation {
 
-	public void nextPermutation(int[] nums) {
+	public static void nextPermutation(int[] nums) {
 		int i = nums.length - 2;
 		while (i >= 0 && nums[i + 1] <= nums[i])
 			i--;
@@ -23,9 +21,35 @@ public class NextPermutation {
 		}
 	}
 
+	public static void nextPermutationAgain(int[] nums) {
+		if (nums == null || nums.length <= 1)
+			return;
+		int i = nums.length - 2;
+		while (i >= 0 && nums[i] >= nums[i + 1])
+			i--;
+		if (i >= 0) {
+			int j = nums.length - 1;
+			while (nums[j] <= nums[i])
+				j--;
+			swap(nums, i, j);
+		}
+		reverse(nums, i + 1, nums.length - 1);
+	}
+
+	public static void swap(int[] A, int i, int j) {
+		int tmp = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
+	}
+
+	public static void reverse(int[] A, int i, int j) {
+		while (i < j)
+			swap(A, i++, j--);
+	}
+
 	public static void main(String[] args) {
 		int[] nums = new int[] { 5, 4, 7, 5, 3, 2 };
-		new NextPermutation().nextPermutation(nums);
+		nextPermutationAgain(nums);
 		for (int i : nums)
 			System.out.print(i);
 	}
