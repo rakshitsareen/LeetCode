4d01a7da475ef7c5392a061ed4c3ade86c9c911d
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-19 21:58:28 -0500

    src/com/leetcode/Permutations.java

---
+++ src/com/leetcode/Permutations.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+	// Given a collection of distinct integers, return all possible permutations.
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		backtrack(res, new ArrayList<Integer>(), nums);
+		return res;
+	}
+	
+	private void backtrack(List<List<Integer>> res, List<Integer> list, int[] nums) {
+		if(list.size() == nums.length) {res.add(new ArrayList<Integer>(list)); return;}
+		else {
+			for(int i = 0; i < nums.length; ++i) {
+				if(list.contains(a)) continue;
+				int a = nums[i];
+				list.add(a);
+				backtrack(res, list, nums);
+				list.remove(list.size() - 1);
+			}
+		}
+	}
+
+}
