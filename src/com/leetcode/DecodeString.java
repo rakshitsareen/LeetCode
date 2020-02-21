7fe47a403b9b3739e21b73b9a060f888bd55b049
Rakshit Sareen <raks.sareen@gmail.com>
2020-02-20 22:07:35 -0500

    src/com/leetcode/DecodeString.java

--- src/com/leetcode/DecodeString.java
+++ src/com/leetcode/DecodeString.java
@@ -3,6 +3,7 @@ package com.leetcode;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class DecodeString {
 
@@ -35,8 +36,41 @@ public class DecodeString {
 		return str.toString();
 	}
 
+	public String decodeString(String s) {
+		Stack<Integer> countStack = new Stack<Integer>();
+		Stack<String> resStack = new Stack<String>();
+		String res = "";
+		int idx = 0;
+		while (idx < s.length()) {
+			if (Character.isDigit(s.charAt(idx))) {
+				int count = 0;
+				while (Character.isDigit(s.charAt(idx))) {
+					count = count * 10 + (s.charAt(idx) - '0');
+					idx++;
+				}
+				countStack.push(count);
+			}
+
+			else if (s.charAt(idx) == '[') {
+				resStack.push(res);
+				res = "";
+				idx++;
+			} else if (s.charAt(idx) == ']') {
+				StringBuilder temp = new StringBuilder(resStack.pop());
+				int count = countStack.pop();
+				for (int i = 0; i < count; ++i)
+					temp.append(res);
+				res = temp.toString();
+				idx++;
+			} else {
+				res += s.charAt(idx++);
+			}
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
-		String s = "3[a]2[bc]6[d]";
-		System.out.println(new DecodeString().wrong_decodeString_wrong(s));
+		String s = "3[a5[pc]]2[bc]6[d]";
+		System.out.println(new DecodeString().decodeString(s));
 	}
 }
