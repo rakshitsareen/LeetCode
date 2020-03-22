a169a86d1ad7dcf89b4e6e39eef5dc823b789166
Rakshit Sareen <raks.sareen@gmail.com>
2020-03-22 15:04:26 -0400

    src/com/leetcode/FindtheCelebrity.java

---
+++ src/com/leetcode/FindtheCelebrity.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+interface Relation {
+	public boolean knows(int i, int j);
+}
+
+public abstract class FindtheCelebrity implements Relation {
+
+	public int findCelebrity(int n) {	// my initial implementation
+		int[] kno = new int[n];
+		int[] onk = new int[n];
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j) {
+				if (j == i)
+					continue;
+				if (knows(i, j)) {
+					// System.out.println(i + " knows " + j);
+					kno[j]++;
+					onk[i]++;
+				}
+			}
+		for (int i = 0; i < n; ++i)
+			if (kno[i] == n - 1 && onk[i] == 0)
+				return i;
+		return -1;
+	}
+
+}
