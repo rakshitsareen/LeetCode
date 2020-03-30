aea49a6789d3282a67823415c4aca476e1010afa
Rakshit Sareen <raks.sareen@gmail.com>
2020-03-30 14:04:08 -0400

    src/com/leetcode/GuesstheWord.java

--- src/com/leetcode/GuesstheWord.java
+++ src/com/leetcode/GuesstheWord.java
@@ -1,49 +1,36 @@
-//package com.leetcode;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//import javafx.util.Pair;
-//
-//interface Master {
-//	public int guess(String word);
-//}
-//
-//public class GuesstheWord {
-//
-//	public int match(String a, String b) {
-//		int matches = 0;
-//		for (int i = 0; i < a.length(); ++i)
-//			if (a.charAt(i) == b.charAt(i))
-//				matches++;
-//		return matches;
-//	}
-//
-//	public void findSecretWord(String[] wordlist, Master master) {
-//		for (int i = 0, x = 0; i < 10 && x < 6; i++) {
-//			HashMap<String, Integer> map = new HashMap<>();
-//			for (String w1 : wordlist)
-//				for (String w2 : wordlist) {
-//					if (match(w1, w2) == 0)
-//						map.put(w1, map.getOrDefault(w1, 0) + 1);
-//				}
-//			Pair<String, Integer> minimax = new Pair<String, Integer>("", 1000);
-//			for (String w : wordlist)
-//				if (map.getOrDefault(w, 0) < minimax.getValue())
-//					minimax = new Pair<String, Integer>(w, map.get(w));
-//			x = master.guess(minimax.getKey());
-//			List<String> wordlist2 = new ArrayList<>();
-//			for (String w : wordlist)
-//				if (match(w, minimax.getKey()) == x)
-//					wordlist2.add(w);
-//			wordlist = wordlist2.toArray(new String[0]);
-//		}
-//	}
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+interface Master {
+	public int guess(String word);
+}
+
+public class GuesstheWord {
+	public void findSecretWord(String[] wordlist, Master master) {
+		for (int i = 0; i < 10; ++i) {
+			String guess = wordlist[new Random().nextInt(wordlist.length)];
+			List<String> list = new ArrayList<String>();
+			int x = master.guess(guess);
+			if (x == 6)
+				return;
+			for (String w : wordlist) {
+				if (match(guess, w) == x) {
+					list.add(w);
+				}
+			}
+			wordlist = list.toArray(new String[list.size()]);
+		}
+	}
+
+	private int match(String a, String b) {
+		int matches = 0;
+		for (int i = 0; i < a.length(); ++i)
+			if (a.charAt(i) == b.charAt(i))
+				matches++;
+		return matches;
+	}
+
+}
