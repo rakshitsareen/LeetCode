2c1005d01b949a3b457d8ccecd3871fd67151230
Rakshit Sareen <raks.sareen@gmail.com>
2020-03-31 14:23:46 -0400

    src/com/leetcode/KokoEatingBananas.java

---
+++ src/com/leetcode/KokoEatingBananas.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+public class KokoEatingBananas {
+	public int minEatingSpeed(int[] piles, int H) {
+		int maxpile = getMaxPile(piles);
+		System.out.println(maxpile);
+		int lo = 1, hi = maxpile;
+		while (lo <= hi) {
+			int mid = (lo + hi) / 2;
+			if (canEatAll(piles, mid, H))
+				hi = mid - 1;
+			else
+				lo = mid + 1;
+		}
+		return lo;
+	}
+
+	private boolean canEatAll(int[] piles, int k, int H) {
+		int hours = 0;
+		for (int pile : piles) {
+			hours += pile / k;
+			if (pile % k != 0) {
+				hours++;
+			}
+		}
+		if (hours <= H)
+			return true;
+		return false;
+	}
+
+	private int getMaxPile(int[] piles) {
+		int output = 0;
+		for (int pile : piles) {
+			output = Math.max(pile, output);
+		}
+		return output;
+	}
+}
