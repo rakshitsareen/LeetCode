b2f98d2a16cb063704e40138fd96a8e282f034a2
rakshit sareen <raks.sareen@gmail.com>
2020-04-26 00:12:01 -0400

    src/com/leetcode/InsertInterval.java

--- src/com/leetcode/InsertInterval.java
+++ src/com/leetcode/InsertInterval.java
@@ -1,5 +1,23 @@
 package com.leetcode;
 
-public class InsertInterval {
+import java.util.ArrayList;
+import java.util.List;
 
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int len = intervals.length;
+        List<int[]> result = new ArrayList<int[]>();
+        int i = 0; 
+        while(i < len && intervals[i][1] < newInterval[0]){
+            result.add(intervals[i++]);
+        }
+        while(i < len && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            ++i;
+        }
+        result.add(newInterval);
+        while(i < len) result.add(intervals[i++]);
+        return result.toArray(new int[result.size()][2]);
+    }
 }
