de00302e2d658b1e36ab3c43eef0f228b43c2888
Rakshit Sareen <rs5606@nyu.edu>
2020-12-22 00:57:34 -0500

    wordladder using level variable

--- src/com/leetcode/WordLadder.java
+++ src/com/leetcode/WordLadder.java
@@ -48,6 +48,36 @@ public class WordLadder {
 		return 0;
 	}
 
+	public int ladderLengthUsingLevelVariable(String start, String end, List<String> wordList) {
+		Set<String> dict = new HashSet<>(wordList);
+		dict.add(start);
+		// Map<String, Set<String>> map = new HashMap<>();
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		Queue<String> q = new LinkedList<>();
+		q.offer(start);
+		Set<String> visited = new HashSet<>();
+		int level = 1;
+		while(!q.isEmpty()){
+			int size = q.size();
+			while(size-- > 0){
+				String curr = q.poll();
+				int n = curr.length();
+				for(int i = 0; i < n; ++i){
+					for(char c : alphabet){
+						String nString = curr.substring(0,i) + c + curr.substring(i+1);
+						if(!visited.contains(nString) && dict.contains(nString)){
+							if(nString.equals(end)) return level + 1;
+							visited.add(nString);
+							q.offer(nString);
+						}
+					}
+				}
+			}
+			level++;
+		}
+		return 0;
+	}
+
 	public static void main(String[] args) {
 		List<String> wordList = new ArrayList<>();
 		// "hot","dot","dog","lot","log","cog"
