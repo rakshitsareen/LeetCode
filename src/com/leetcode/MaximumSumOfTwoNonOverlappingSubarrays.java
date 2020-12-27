1ecd3fcc89337239c2306d06dd3c21bdb288f16a
Rakshit Sareen <rs5606@nyu.edu>
2020-12-27 15:16:19 -0500

    max sum non overlap arrays +ve integers

--- src/com/leetcode/MaximumSumOfTwoNonOverlappingSubarrays.java
+++ src/com/leetcode/MaximumSumOfTwoNonOverlappingSubarrays.java
@@ -1,6 +1,9 @@
 package com.leetcode;
 
 
+import sun.nio.cs.ext.MacHebrew;
+import sun.print.DialogOnTop;
+
 public class MaximumSumOfTwoNonOverlappingSubarrays {
     public int maxSumTwoNoOverlap(int[] A, int L, int M) {
         for(int i = 1; i < A.length; ++i){
@@ -17,9 +20,31 @@ public class MaximumSumOfTwoNonOverlappingSubarrays {
         }
         return ans;
     }
+
+    public int maxSumTwoNoOverlap_slidingWindow(int[] A, int L, int M) {
+        return Math.max(helper(A, L, M), helper(A, M, L));
+    }
+
+    private int helper(int[] A, int L, int M){
+        int sumL = 0, sumM = 0;
+        for(int i = 0; i < L + M; i++){
+            if(i < L) sumL += A[i];
+            else sumM += A[i];
+        }
+        int ans = sumL + sumM;
+        int maxL = sumL;
+        for(int i = L + M; i < A.length; i++){
+            sumL += A[i - M] - A[i - M - L];
+            sumM += A[i] - A[i - M];
+            maxL = Math.max(maxL, sumL);
+            ans = Math.max(ans, maxL + sumM);
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         int[] arr = {1,0,3};
         MaximumSumOfTwoNonOverlappingSubarrays sol = new MaximumSumOfTwoNonOverlappingSubarrays();
-        System.out.println(sol.maxSumTwoNoOverlap(arr,1,2));
+        System.out.println(sol.maxSumTwoNoOverlap_slidingWindow(arr,1,2));
     }
 }
