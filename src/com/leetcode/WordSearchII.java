f1e75c6464ba24048e22bdd977cd7a470a5874ba
Rakshit Sareen <rs5606@nyu.edu>
2021-01-28 19:55:16 -0500

    word search 2

--- src/com/leetcode/WordSearchII.java
+++ src/com/leetcode/WordSearchII.java
@@ -4,55 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WordSearchII {
+	static class TrieNode{
+		TrieNode[] next = new TrieNode[26];
+		String word;
+	}
 
 	public List<String> findWords(char[][] board, String[] words) {
-		List<String> res = new ArrayList<String>();
+		List<String> res = new ArrayList<>();
 		TrieNode root = buildTrie(words);
-		for (int i = 0; i < words.length; ++i)
-			for (int j = 0; j < words[0].length(); ++j)
-				dfs(board, i, j, root, res);
+		for(int i = 0; i < board.length; ++i){
+			for(int j = 0; j < board[0].length; ++j){
+				dfs(root, i, j, board, res);
+			}
+		}
 		return res;
 	}
 
-	private void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-		char c = board[i][j];
-		if (c == '#' || p.next[c - 'a'] == null)
-			return;
-		p = p.next[c - 'a'];
-		if (p.word != null) {
-			res.add(p.word);
-			p.word = null;
+	public void dfs(TrieNode root, int i, int j, char[][] board, List<String> res){
+		char letter = board[i][j];
+		if(letter == '#' || root.next[letter - 'a'] == null) return;
+		root = root.next[letter - 'a'];
+		if(root.word != null){
+			res.add(root.word);
+			root.word = null;
 		}
 		board[i][j] = '#';
-
-		if (i > 0)
-			dfs(board, i - 1, j, p, res);
-		if (j > 0)
-			dfs(board, i, j - 1, p, res);
-		if (i < board.length - 1)
-			dfs(board, i + 1, j, p, res);
-		if (j < board[0].length - 1)
-			dfs(board, i, j + 1, p, res);
-		board[i][j] = c;
+		if(i > 0) dfs(root, i - 1, j, board, res);
+		if(j > 0) dfs(root, i, j - 1, board, res);
+		if(i < board.length - 1) dfs(root, i + 1, j, board, res);
+		if(j < board[0].length - 1) dfs(root, i, j + 1, board, res);
+		board[i][j] = letter;
 	}
 
-	private TrieNode buildTrie(String[] words) {
+	public TrieNode buildTrie(String[] words){
 		TrieNode root = new TrieNode();
-		for (String w : words) {
+		for(String w : words){
 			TrieNode p = root;
-			for (char c : w.toCharArray()) {
-				if (p.next[c - 'a'] == null)
-					p.next[c - 'a'] = new TrieNode();
-				p = p.next[c - 'a'];
+			for(Character character : w.toCharArray()){
+				if(p.next[character - 'a'] == null) p.next[character - 'a'] = new TrieNode();
+				p = p.next[character - 'a'];
 			}
 			p.word = w;
 		}
 		return root;
 	}
-
-	class TrieNode {
-		TrieNode[] next = new TrieNode[26];
-		String word;
-	}
-
 }
