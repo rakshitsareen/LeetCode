a34052dbf2d3eaf067e3abb3585d91a929140d24
Rakshit Sareen <rs5606@nyu.edu>
2021-03-27 17:39:25 -0400

    src/com/leetcode/OneEditDistance.java

---
+++ src/com/leetcode/OneEditDistance.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+public class OneEditDistance {
+    public boolean isOneEditDistance(String s, String t) {
+        for(int i = 0; i < Math.min(s.length(), t.length()); ++i){
+            if(s.charAt(i) != t.charAt(i)){
+                if(s.length() == t.length()){
+                    return s.substring(i+1).equals(t.substring(i+1));
+                }
+                if(s.length() > t.length()){
+                    return s.substring(i+1).equals(t.substring(i));
+                }
+                if(s.length() < t.length()){
+                    return s.substring(i).equals(t.substring(i+1));
+                }
+            }
+        }
+        return Math.abs(s.length() - t.length()) == 1;
+    }
+}
