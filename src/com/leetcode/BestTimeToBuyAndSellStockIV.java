24288ef2372620b80026e0a35e6952b631c398d6
Rakshit Sareen <rs5606@nyu.edu>
2021-04-11 20:01:48 -0400

    src/com/leetcode/BestTimeToBuyAndSellStockIV.java

---
+++ src/com/leetcode/BestTimeToBuyAndSellStockIV.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import com.sun.deploy.panel.ITreeNode;
+
+public class BestTimeToBuyAndSellStockIV {
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        if(k >= len / 2) return quickSolve(prices);
+        int[][] dp = new int[k + 1][len];
+        for(int i = 1; i <= k; ++i){
+            int tmpMax = -prices[0];
+            for(int j = 1; j < len; ++j){
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + tmpMax);
+                tmpMax = Math.max(tmpMax, dp[i-1][j-1] - prices[j]);
+            }
+        }
+        return dp[k][len - 1];
+    }
+
+    private int quickSolve(int[] prices){
+        int profit = 0;
+        for(int i = 1; i < prices.length; ++i){
+            if(prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
+        }
+        return profit;
+    }
+}
