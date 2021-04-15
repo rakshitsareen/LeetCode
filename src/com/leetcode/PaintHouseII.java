a2f2accb3aaf46e0b3572605643ca85ac68a4df4
Rakshit Sareen <rs5606@nyu.edu>
2021-04-14 20:33:54 -0400

    src/com/leetcode/PaintHouseII.java

---
+++ src/com/leetcode/PaintHouseII.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
+
+public class PaintHouseII {
+    public int minCostII(int[][] costs) {
+        if(costs == null || costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+        for(int house = 1; house < n; ++house){
+            for(int color = 0; color < k; color++){
+                int min = Integer.MAX_VALUE;
+                for(int prevcolor = 0; prevcolor < k; ++prevcolor){
+                    if(color == prevcolor) continue;
+                    min = Math.min(min, costs[house - 1][prevcolor]);
+                }
+                costs[house][color] += min;
+            }
+        }
+        int output = Integer.MAX_VALUE;
+        for(int i = 0; i < k; ++i){
+            output = Math.min(output, costs[costs.length - 1][i]);
+        }
+        return output;
+    }
+}
